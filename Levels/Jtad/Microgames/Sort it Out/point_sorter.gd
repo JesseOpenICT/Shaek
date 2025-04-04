de908b2f54de6e3d799bc1dafc6312dacc8459ef
JesseOpenICT <125012405+JesseOpenICT@users.noreply.github.com>
2025-04-04 09:40:26 +0200

    Visual feedback sort game

--- Levels/Jtad/Microgames/Sort it Out/point_sorter.gd
+++ Levels/Jtad/Microgames/Sort it Out/point_sorter.gd
@@ -7,6 +7,10 @@ var order : Array[int]
 
 const ORDER_OPTIONS = [[1,3,0,2], [2,0,1,3], [0,3,1,2]]
 
+@export var off : CompressedTexture2D = preload("res://Levels/Jtad/Microgames/Sort it Out/bgoff.png")
+@export var on : CompressedTexture2D = preload("res://Levels/Jtad/Microgames/Sort it Out/bgon.png")
+@onready var bg = $"../Bg"
+
 func _ready() -> void:
 	order.append_array( ORDER_OPTIONS[randi_range(1,ORDER_OPTIONS.size())-1] )
 	var index = 0
@@ -31,3 +35,4 @@ func _process(_delta: float) -> void:
 					await get_tree().process_frame
 					await get_tree().process_frame
 					$"..".win = (order in [[0,1,2,3], [3,2,1,0]])
+					bg.texture = on if $"..".win else off
