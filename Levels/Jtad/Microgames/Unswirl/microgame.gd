236e8a0965589f95e155500654e3c1eed4539f18
JesseOpenICT <125012405+JesseOpenICT@users.noreply.github.com>
2025-06-27 14:18:58 +0200

    Unswirl finished
    It works 👍

--- Levels/Jtad/Microgames/Unswirl/microgame.gd
+++ Levels/Jtad/Microgames/Unswirl/microgame.gd
@@ -1,14 +1,14 @@
 extends Microgame
 
 
-var swirl : float = -3
+var swirl : float = -7.5
 const CENTRE = Vector2(128, 96)
 var wincon_positive : bool
 
 
 func _ready() -> void:
 	super()
-	swirl = [-5,5][randi_range(0,1)]
+	swirl = [swirl,0-swirl][randi_range(0,1)]
 	$StargazingDown.material.set("shader_parameter/warp", swirl)
 	wincon_positive = swirl < 0
 	$StargazingDown/Arrow.scale.x = -1 if wincon_positive else 1
@@ -25,12 +25,17 @@ func _input(event: InputEvent) -> void:
 		end_coords = (end_coords-CENTRE).rotated(-start_angle) + CENTRE
 		var end_angle = CENTRE.angle_to_point(end_coords)
 		
+		
+		if (end_angle < 0 and swirl > 0 ) or (end_angle > 0 and swirl < 0 ):
+			end_angle = end_angle/4
+		
+		
 		swirl -= end_angle / 3
-		if (wincon_positive and swirl >= 0) or (!wincon_positive and swirl <= 0):
+		if (wincon_positive and swirl >= -1) or (!wincon_positive and swirl <= 1):
 			swirl = 0
 			win = true
 			var tween : Tween = create_tween()
 			tween.tween_property($StargazingDown/Arrow, "modulate", Color.TRANSPARENT, .5)
 			tween.parallel().tween_property($StargazingDown/Arrow, "scale", $StargazingDown/Arrow.scale*2, .5)
 		
-		$StargazingDown.material.set("shader_parameter/warp", swirl)
+		$StargazingDown.material.set("shader_parameter/warp", .01*pow(swirl, 3))
