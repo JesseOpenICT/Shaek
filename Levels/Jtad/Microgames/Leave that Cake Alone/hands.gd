35f8e9f5b0a7585cfe06bb16f80d5d36e4e0b2e4
JesseOpenICT <125012405+JesseOpenICT@users.noreply.github.com>
2025-07-01 12:10:07 +0200

    Really funny microgame
    hhhehehe ( one to go btw )

---
+++ Levels/Jtad/Microgames/Leave that Cake Alone/hands.gd
@@ -0,0 +1,33 @@
+extends Sprite2D
+
+var traveled_distance : float:
+	set(value):
+		traveled_distance = value
+		arc = 128 - 198 * pow(traveled_distance-1, 2)
+		position.x = arc
+		if traveled_distance > 1:
+			$"..".win = false
+			$"../Cake".position.x = arc
+
+var grappled : bool
+var arc : int
+
+
+func _ready() -> void:
+	$"..".win = true
+
+
+func grapple():
+	if not grappled:
+		$"../Yoink".pitch_scale = $"..".speed * .55
+		$"../Yoink".play()
+		grappled = true
+		var tween : Tween = create_tween()
+		tween.tween_property(self, "traveled_distance", 3., 1.5/$"..".speed)
+		await tween.finished
+		$"../OhNoes".visible = true
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("left_mouse"):
+		grapple()
